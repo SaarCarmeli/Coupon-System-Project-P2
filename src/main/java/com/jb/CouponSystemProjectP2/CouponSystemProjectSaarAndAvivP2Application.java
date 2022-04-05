4a1855ff2ee1d72c935b56eb42f5904478402554
SaarCarmeli <carmeli.saar@gmail.com>
2022-04-05 19:19:46 +0300

    First commit

---
+++ src/main/java/com/jb/CouponSystemProjectP2/CouponSystemProjectSaarAndAvivP2Application.java
@@ -0,0 +1,13 @@
+package com.jb.CouponSystemProjectP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponSystemProjectSaarAndAvivP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponSystemProjectSaarAndAvivP2Application.class, args);
+	}
+
+}

6dff5583b81a904ae18513a2b2dfa0318e86cad4
Saar <carmeli.saar@gmail.com>
2022-04-20 18:40:19 +0300

    added deleteByEndDateAfter method for CouponRepository

--- src/main/java/com/jb/CouponSystemProjectP2/Repository/CouponRepository.java
+++ src/main/java/com/jb/CouponSystemProjectP2/Repository/CouponRepository.java
@@ -3,5 +3,9 @@ package com.jb.CouponSystemProjectP2.Repository;
 import com.jb.CouponSystemProjectP2.Beans.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Date;
+
 public interface CouponRepository extends JpaRepository<Coupon,Integer> {
+
+    void deleteByEndDateAfter(Date endDate);
 }

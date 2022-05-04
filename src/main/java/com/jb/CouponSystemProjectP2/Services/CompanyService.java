62123c211b3872a4e45c5dc7a6c20e03a60ba245
Saar <carmeli.saar@gmail.com>
2022-05-04 19:23:20 +0300

    added some of the methods for CompanyService

--- src/main/java/com/jb/CouponSystemProjectP2/Services/CompanyService.java
+++ src/main/java/com/jb/CouponSystemProjectP2/Services/CompanyService.java
@@ -3,7 +3,9 @@ package com.jb.CouponSystemProjectP2.Services;
 import com.jb.CouponSystemProjectP2.Beans.Category;
 import com.jb.CouponSystemProjectP2.Beans.Company;
 import com.jb.CouponSystemProjectP2.Beans.Coupon;
+import com.jb.CouponSystemProjectP2.Exceptions.CompanyException;
 import com.jb.CouponSystemProjectP2.Repositories.CompanyRepository;
+import com.jb.CouponSystemProjectP2.Repositories.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,26 +15,47 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CompanyService implements CompanyServiceDAO {
     private final CompanyRepository companyRepository;
+    private final CouponRepository couponRepository;
     private int loggedCompanyId;
 
     @Override
-    public void createCoupon(Coupon coupon) {
-
+    public void createCoupon(Coupon coupon) throws CompanyException {
+        if (couponRepository.existsByTitleAndCompanyId(coupon.getTitle(), this.loggedCompanyId)) {
+            throw new CompanyException(
+                    "Failed to create 'coupon', as 'coupon' by title= " + coupon.getTitle() + ", and by company_Id= " + this.loggedCompanyId + " already exists!");
+        }
+        Company loggedCompany = companyRepository.getById(loggedCompanyId);
+        List<Coupon> companyCoupons = loggedCompany.getCoupons();
+        companyCoupons.add(coupon);
+        loggedCompany.setCoupons(companyCoupons);
+        companyRepository.save(loggedCompany); // todo check if does not create duplicate coupons
     }
 
     @Override
-    public List<Coupon> readAllCompanyCoupons() {
-        return null;
+    public List<Coupon> readAllCompanyCoupons() throws CompanyException {
+        List<Coupon> couponList = couponRepository.findByCompanyId(this.loggedCompanyId);
+        if (!couponList.isEmpty()) {
+            return couponList;
+        }
+        throw new CompanyException("Failed to read 'company' coupons, as 'company' did not issue any!");
     }
 
     @Override
-    public List<Coupon> readCompanyCouponsByCategory(Category category) {
-        return null;
+    public List<Coupon> readCompanyCouponsByCategory(Category category) throws CompanyException {
+        List<Coupon> couponList = couponRepository.findByCompanyIdAndCategory(this.loggedCompanyId, category);
+        if (!couponList.isEmpty()) {
+            return couponList;
+        }
+        throw new CompanyException("Failed to read 'company' coupons, as 'company' did not issue any 'coupon' of category= " + category + "!");
     }
 
     @Override
-    public List<Coupon> readCompanyCouponsByMaxPrice(double price) {
-        return null;
+    public List<Coupon> readCompanyCouponsByMaxPrice(double price) throws CompanyException {
+        List<Coupon> couponList = couponRepository.findByCompanyIdAndPriceLessThan(this.loggedCompanyId, price);
+        if (!couponList.isEmpty()) {
+            return couponList;
+        }
+        throw new CompanyException("Failed to read 'company' coupons, as 'company' did not issue any 'coupon' under price= " + price + "!");
     }
 
     @Override
@@ -47,6 +70,6 @@ public class CompanyService implements CompanyServiceDAO {
 
     @Override
     public Company readCompanyDetails() {
-        return null;
+        return companyRepository.getById(this.loggedCompanyId);
     }
 }
--- src/main/java/com/jb/CouponSystemProjectP2/Services/CompanyServiceDAO.java
+++ src/main/java/com/jb/CouponSystemProjectP2/Services/CompanyServiceDAO.java
@@ -3,14 +3,15 @@ package com.jb.CouponSystemProjectP2.Services;
 import com.jb.CouponSystemProjectP2.Beans.Category;
 import com.jb.CouponSystemProjectP2.Beans.Company;
 import com.jb.CouponSystemProjectP2.Beans.Coupon;
+import com.jb.CouponSystemProjectP2.Exceptions.CompanyException;
 
 import java.util.List;
 
 public interface CompanyServiceDAO {
-    void createCoupon(Coupon coupon);
-    List<Coupon> readAllCompanyCoupons();
-    List<Coupon> readCompanyCouponsByCategory(Category category);
-    List<Coupon> readCompanyCouponsByMaxPrice(double price);
+    void createCoupon(Coupon coupon) throws CompanyException;
+    List<Coupon> readAllCompanyCoupons() throws CompanyException;
+    List<Coupon> readCompanyCouponsByCategory(Category category) throws CompanyException;
+    List<Coupon> readCompanyCouponsByMaxPrice(double price) throws CompanyException;
     void updateCoupon(Coupon coupon);
     void deleteCouponById(int id);
     Company readCompanyDetails();

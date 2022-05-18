4b2486bfedc96aaac8dbc2b119155b95881457d0
Saar <carmeli.saar@gmail.com>
2022-05-18 19:13:34 +0300

    fixed findCouponByCategory methods for customer and company in CouponRepository to receive String parameter instead of Enum, fixed Services accordingly to pass String to respective methods

--- src/main/java/com/jb/CouponSystemProjectP2/Repositories/CouponRepository.java
+++ src/main/java/com/jb/CouponSystemProjectP2/Repositories/CouponRepository.java
@@ -19,7 +19,7 @@ public interface CouponRepository extends JpaRepository<Coupon, Integer> {
     List<Coupon> findByCompanyId(@Param("companyId") int companyId);
 
     @Query(value = "SELECT * FROM `coupon_project_p2`.`coupons` WHERE `company_id` = :companyId AND `category` = :category", nativeQuery = true)
-    List<Coupon> findByCompanyIdAndCategory(@Param("companyId") int companyId, @Param("category") Category category);
+    List<Coupon> findByCompanyIdAndCategory(@Param("companyId") int companyId, @Param("category") String category);
 
     @Query(value = "SELECT * FROM `coupon_project_p2`.`coupons` WHERE `company_id` = :companyId AND `price` < :price", nativeQuery = true)
     List<Coupon> findByCompanyIdAndPriceLessThan(@Param("companyId") int companyId, @Param("price") double price);
@@ -36,7 +36,7 @@ public interface CouponRepository extends JpaRepository<Coupon, Integer> {
             "JOIN `coupon_project_p2`.`customer_coupons` AS cc ON cc.coupons_coupon_id = c.coupon_id" +
             "WHERE cc.customer_customer_id = :customerId AND c.category = :category",
             nativeQuery = true)
-    List<Coupon> findByCustomerIdAndCategory(@Param("customerId") int customerId, @Param("category") Category category);
+    List<Coupon> findByCustomerIdAndCategory(@Param("customerId") int customerId, @Param("category") String category);
 
     @Query(value = "SELECT c.coupon_id, c.amount, c.category, c.description, c.end_date, c.image, c.price, c.start_date, c.title, c.company_id" +
             "FROM `coupon_project_p2`.`coupons` AS c " +

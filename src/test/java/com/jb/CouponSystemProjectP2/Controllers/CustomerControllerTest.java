9ce88d0da67ae005d27f8a185bcdf3c09389b543
aviv-mond <avivmond1@gmail.com>
2022-05-22 22:06:17 +0300

    added testing dependencies,and Testing Classes.

---
+++ src/test/java/com/jb/CouponSystemProjectP2/Controllers/CustomerControllerTest.java
@@ -0,0 +1,28 @@
+package com.jb.CouponSystemProjectP2.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerControllerTest {
+
+    @Test
+    void getAllCustomerCoupons() {
+    }
+
+    @Test
+    void getCustomerCouponsByCategory() {
+    }
+
+    @Test
+    void getCustomerCouponsByMaxPrice() {
+    }
+
+    @Test
+    void getCustomerDetail() {
+    }
+
+    @Test
+    void purchaseCoupon() {
+    }
+}
--- src/test/java/com/jb/CouponSystemProjectP2/Services/AdministratorServiceTest.java
+++ src/test/java/com/jb/CouponSystemProjectP2/Services/AdministratorServiceTest.java
@@ -6,53 +6,58 @@ import com.jb.CouponSystemProjectP2.Exceptions.CompanyNotFoundException;
 import com.jb.CouponSystemProjectP2.Repositories.CompanyRepository;
 import com.jb.CouponSystemProjectP2.Repositories.CustomerRepository;
 import com.jb.CouponSystemProjectP2.Util.TablePrinter;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+
+
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 
-class AdministratorServiceTest {
+class AdministratorServiceTest extends BaseServiceTest {
 
     @Mock
     private CompanyRepository companyRepository;
+    @Mock
     private CustomerRepository customerRepository;
+    @InjectMocks
     private AdministratorService underTest;
-    private AutoCloseable autoCloseable;
 
-    @BeforeEach
-    void setUp() {
-        autoCloseable = MockitoAnnotations.openMocks(this);
-        underTest = new AdministratorService(companyRepository, customerRepository);
-    }
-
-    @AfterEach
-    void tearDown() throws Exception {
-        autoCloseable.close();
-    }
 
     @Test
     void createCompany() throws CompanyException, CompanyNotFoundException {
         //given
-        Company amazon = Company.builder()
-                .id(1)
+        Company mockCompany = Company.builder()
                 .name("Amazon")
-                .email("amazon@amazon.com")
+                .email("mockCompany@mockCompany.com")
                 .password("jungle")
                 .build();
-        underTest.createCompany(amazon);
-        TablePrinter.print(amazon);
-
-        //when
-        Company mockCompany = underTest.readCompanyById(amazon.getId());
-        //then
-        assertThat(amazon).isEqualTo(mockCompany);
+        when(companyRepository.existsByNameOrEmail(any(),any())).thenReturn(false);
+        //what we test
+        underTest.createCompany(mockCompany);
+        verify(companyRepository,times(1)).existsByNameOrEmail(mockCompany.getName(),mockCompany.getEmail());
+        verify(companyRepository,times(1)).save(mockCompany);
     }
 
+    @Test
+    void failedToCreateCompany() throws CompanyException, CompanyNotFoundException {
+        //given
+        Company mockCompany = Company.builder()
+                .name("Amazon")
+                .email("mockCompany@mockCompany.com")
+                .password("jungle")
+                .build();
+        when(companyRepository.existsByNameOrEmail(any(),any())).thenReturn(true);
+        //what we test
+        Assertions.assertThrows(CompanyException.class,
+                ()->underTest.createCompany(mockCompany));
+        verify(companyRepository,times(1)).existsByNameOrEmail(mockCompany.getName(),mockCompany.getEmail());
+        verify(companyRepository,times(0)).save(mockCompany);
+    }
     @Test
     void readCompanyById() {
     }
@@ -60,9 +65,9 @@ class AdministratorServiceTest {
     @Test
     void canReadAllCompanies() throws CompanyNotFoundException {
         //when
-        underTest.readAllCompanies();
+        String url = "http://localhost:8080/admin/all/companies";
         //then
-        verify(companyRepository).findAll();
+
     }
 
     @Test
